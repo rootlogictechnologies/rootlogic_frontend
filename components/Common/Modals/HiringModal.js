53541167cfcd127aca90a03d85d737b86f842222
AsuKaur <asukaur99@gmail.com>
2021-12-12 21:03:05 +0530

    Hiring Modal

--- components/Common/Modals/HiringModal.js
+++ components/Common/Modals/HiringModal.js
@@ -1,11 +1,18 @@
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { XIcon } from "@heroicons/react/outline";
+import { UploadIcon, XIcon } from "@heroicons/react/outline";
 import Image from "next/image";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
 
 // Assets
 import ModalGraphic from "assets/Career/ModalGraphic.svg";
 
+// TODO: Add validations and upload feature
 function HiringModal({ open, setOpen }) {
   return (
     <>
@@ -55,15 +62,89 @@ function HiringModal({ open, setOpen }) {
                     <XIcon className="h-4 w-4" aria-hidden="true" />
                   </button>
                 </div>
-                <div className="flex flex-row sm:items-center w-full">
-                  <div className="w-1/2 bg-rl-light px-12 py-24 hidden md:block">
+                <div className="flex flex-row sm:items-center w-full h-full bg-rl-light">
+                  <div className="w-1/2 bg-rl-light px-12 py-24 hidden md:block h-full">
                     <Image src={ModalGraphic} />
                   </div>
-                  <div className="w-full md:w-1/2 flex flex-col items-start px-10 py-16 space-y-7">
-                    <Dialog.Title className="font-semibold text-3.5xl text-left leading-snug text-rl-dark-grey">
+                  <div className="w-full md:w-1/2 flex flex-col items-start px-10 py-16 space-y-7 bg-white">
+                    <Dialog.Title className="font-semibold text-md md:text-3.5xl text-left leading-snug text-rl-dark-grey">
                       Submit your details to apply for this position
                     </Dialog.Title>
-                    <form className=""></form>
+                    <form className="flex flex-col items-start w-full space-y-7">
+                      <TextField
+                        id="name"
+                        label="Name"
+                        type="text"
+                        variant="standard"
+                        className="w-full focus:border-0 focus:border-transparent focus:ring-0 focus:ring-transparent"
+                      />
+                      <TextField
+                        id="email"
+                        label="Email"
+                        type="email"
+                        variant="standard"
+                        className="w-full focus:border-0 focus:border-transparent focus:ring-0 focus:ring-transparent"
+                      />
+                      <TextField
+                        id="phoneNumber"
+                        label="Phone Number"
+                        type="number"
+                        variant="standard"
+                        className="w-full focus:border-0 focus:border-transparent focus:ring-0 focus:ring-transparent"
+                      />
+                      <TextField
+                        id="linkedinProfile"
+                        label="Linkedin Profile"
+                        type="text"
+                        variant="standard"
+                        className="w-full focus:border-0 focus:border-transparent focus:ring-0 focus:ring-transparent"
+                      />
+                      <TextField
+                        id="portfolioLink"
+                        label="Portfolio Link"
+                        type="text"
+                        variant="standard"
+                        className="w-full focus:border-0 focus:border-transparent focus:ring-0 focus:ring-transparent"
+                      />
+                      <TextField
+                        id="githubProfile"
+                        label="Github Profile"
+                        type="text"
+                        variant="standard"
+                        className="w-full focus:border-0 focus:border-transparent focus:ring-0 focus:ring-transparent"
+                      />
+                      <FormControl
+                        className="w-full focus:border-0 focus:border-transparent focus:ring-0 focus:ring-transparent"
+                        variant="standard"
+                      >
+                        <InputLabel htmlFor="uploadResume">
+                          Upload resume
+                        </InputLabel>
+                        <Input
+                          id="uploadResume"
+                          label="Upload Resume"
+                          type="text"
+                          variant="standard"
+                          className="w-full focus:border-0 focus:border-transparent focus:ring-0 focus:ring-transparent"
+                          endAdornment={
+                            <InputAdornment position="end">
+                              <IconButton
+                              // onClick={handleClickShowPassword}
+                              // onMouseDown={handleMouseDownPassword}
+                              >
+                                <UploadIcon className="h-5 w-5" />
+                              </IconButton>
+                            </InputAdornment>
+                          }
+                        />
+                      </FormControl>
+                      <button
+                        type="submit"
+                        className="bg-rl-red text-white font-semibold text-md text-center px-9 py-2.5 rounded-full cursor-pointer transform transition hover:scale-105 duration-300 ease-in-out"
+                      >
+                        Apply Now
+                      </button>
+                    </form>
                   </div>
                 </div>
               </div>
